ad9729e1f30f4bfffeca5817e10b0300619558e7
Repte01 <u0458666111@gmail.com>
2026-01-18 16:18:31 +0100

    Update supabase_client.py

--- services/supabase_client.py
+++ services/supabase_client.py
@@ -1,11 +1,7 @@
-import os
 import streamlit as st
 from supabase import create_client, Client
 
-SUPABASE_URL = st.secrets.get("SUPABASE_URL")
-SUPABASE_KEY = st.secrets.get("SUPABASE_KEY")
-
-if not SUPABASE_URL or not SUPABASE_KEY:
-    raise ValueError("❌ Faltan claves de Supabase")
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
