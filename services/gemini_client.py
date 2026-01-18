13ce34fff51121c767e60ef7ce375cc2085bb363
Repte01 <u0458666111@gmail.com>
2026-01-18 17:16:05 +0100

    Update gemini_client.py

--- services/gemini_client.py
+++ services/gemini_client.py
@@ -1,16 +1,18 @@
-import streamlit as st
+import os
 from google import genai
 
-# Crear cliente oficial (SDK NUEVO)
-client = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])
+# Cliente oficial nuevo
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 
 def gemini_chat(prompt: str) -> str:
     try:
         response = client.models.generate_content(
-            model="gemini-1.5-pro",
+            model="gemini-1.5-flash",
             contents=prompt
         )
+
         return response.text
+
     except Exception as e:
         raise Exception(f"Error Gemini API: {e}")
