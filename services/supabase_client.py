4f9b474d2293bb53c138753d1f975db9267f8f63
Repte01 <u0458666111@gmail.com>
2026-01-18 12:06:19 +0100

    Update supabase_client.py

--- services/supabase_client.py
+++ services/supabase_client.py
@@ -1,12 +1,11 @@
-from supabase import create_client
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
 
-# Cargar variables del .env
+# Cargar variables de entorno
 load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_ANON_KEY")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-# Crear el cliente de Supabase
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
