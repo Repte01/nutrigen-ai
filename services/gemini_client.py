fec03b9efeec015490dfa4c6bd1a5d94e3c19c69
Repte01 <u0458666111@gmail.com>
2026-01-20 16:09:07 +0100

    Update gemini_client.py

--- services/gemini_client.py
+++ services/gemini_client.py
@@ -10,7 +10,7 @@ def gemini_chat(prompt: str) -> str:
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
+        model="gemini-3-flash-preview",
         contents=prompt
     )
 
