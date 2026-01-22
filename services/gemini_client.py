c59037e85e33df4f06a718dc1f9f01056920dcf0
Repte01 <u0458666111@gmail.com>
2026-01-22 11:36:01 +0100

    Update gemini_client.py

--- services/gemini_client.py
+++ services/gemini_client.py
@@ -1,11 +1,17 @@
 import os
 from google import genai
 
+def gemini_chat(prompt: str) -> str:
+    api_key = os.getenv("GEMINI_API_KEY")
 
-def gemini_chat(prompt):
-    client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+    if not api_key:
+        raise Exception("❌ GEMINI_API_KEY no encontrada")
+
+    client = genai.Client(api_key=api_key)
+
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=prompt
     )
+
     return response.text
