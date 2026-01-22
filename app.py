2947574b2e41424bfa584afaf5e15ae929b0f4c0
Repte01 <u0458666111@gmail.com>
2026-01-22 16:18:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from auth.login import login_form, register_form, logout
 from services.gemini_client import gemini_chat
 from services.chat_service import save_chat, get_chat_history, update_chat_title
-from services.pdf_service import generar_pdf_chat
 from services.pdf_reader_service import pdf_to_text
 from services.pdf_chat_service import save_pdf_chat, get_pdf_chat_history
 
